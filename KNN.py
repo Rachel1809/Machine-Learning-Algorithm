98df620f98b9a84909d9d35a298e337ceba71ede
Rachel1809 <ttbtrinh20@apcs.fitus.edu.vn>
2022-09-13 23:28:18 +0700

    update KNN

--- KNN.py
+++ KNN.py
@@ -12,20 +12,15 @@ class KNN:
         self.X_train = X
         self.y_train = y
         
+    def predict (self, X):
+        labels = [self._predict(x) for x in X]
+        return np.array(labels)
     
-    def predict(self, X):
-        predicted_labels = [self._predict(x) for x in X]
-        return np.array(predicted_labels)
-        
     def _predict(self, x):
-        # compute distances
-        distances = [euclidean_distance(x_train, x) for x_train in self.X_train]
-        
-        # get k nearest samples, labels
+        distances = [euclidean_distance(x, x_train) for x_train in self.X_train]
         k_indices = np.argsort(distances)[:self.k]
-        k_nearest_labels = [self.y_train[i] for i in k_indices]
+        k_labels = [self.y_train[i] for i in k_indices]
+        return Counter(k_labels).most_common(1)[0][0]
         
-        # majority vote, most common class label
-        most_common = Counter(k_nearest_labels).most_common(1)
-        return most_common[0][0]
+    
     
